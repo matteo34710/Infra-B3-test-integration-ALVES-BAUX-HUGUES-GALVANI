7c896259e386e276c5c67bf9ee06f9d98e22f174
matobo <74604678+matteo34710@users.noreply.github.com>
2023-03-27 13:51:35 +0200

    Update semabox.py

--- semabox.py
+++ semabox.py
@@ -1,107 +1,445 @@
-```from flask import Flask, render_template, request, redirect, url_for
-import requests
+import threading
+from flask import Flask, jsonify
+import tkinter as tk
 import os
-import ipaddress
+import re
+import time
+import nmap
+import socket
 import subprocess
+import json
+from tkinter import messagebox
+import requests
+from tabulate import tabulate
+import urllib.request
+import ipaddress
 
-app = Flask(__name__)
 
-# Adresse IP des agents
-#agent_ips = [] # A adapter en fonction des adresses IP des agents disponibles
-ips_last_modified = 0
+ip = None
+result_upload= None
+result_download = None
+time_ping = None
+local_ip =  None
+Hostname = socket.gethostname()
+results_str = None
+dns_hostname = None
 
-# Définition de la page d'accueil
-@app.route('/')
-def home():
-    global agent_ips
-    global ips_last_modified
+file_url = "http://192.168.17.131/api/v4/projects/3/repository/files/version.txt/raw?private_token=glpat-HoRSADDiugzHpRxamg6C"
 
-    ips_path = 'ips.txt'
-    ips_last_modified_new = os.stat(ips_path).st_mtime
+# Récupérer le contenu du fichier en utilisant le token privé
+response = requests.get(file_url)
 
-    # Si le fichier a été modifié depuis la dernière requête, on recharge les adresses IP
-    if ips_last_modified != ips_last_modified_new:
-        with open(ips_path) as f:
-            agent_ips = [ip.strip() for ip in f.read().split(',')]
-        ips_last_modified = ips_last_modified_new
+url_content = response.text
 
-    agent_vars = []
-    for agent_ip in agent_ips:
-        agent_url = f'http://{agent_ip}:5000'
+
+def get_local_ip():
+    global local_ip
+    global dns_hostname
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        # connect to a public address, to get the address of the current network
+        s.connect(("8.8.8.8", 1))
+        local_ip = s.getsockname()[0]
+        dns_hostname = socket.gethostbyaddr(local_ip)[0]
+
+    except:
+        local_ip = "127.0.0.1"
+    finally:
+        s.close()
+    return local_ip
+
+
+
+class MainPage(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.grid(sticky="nsew")
+        self.master.geometry("840x300")
+        
+
+        # Create the left frame for information
+        self.left_frame = tk.Frame(self)
+        self.left_frame.grid(row=0, column=0, padx=10, sticky="nsew")
+        # Create the middle frame for buttons
+        self.middle_frame = tk.Frame(self)
+        self.middle_frame.grid(row=0, column=1,padx=60, sticky="nsew")
+        # Create the right frame for the ping
+        self.right_frame = tk.Frame(self)
+        self.right_frame.grid(row=0, column=2,padx=30, sticky="nsew")
+        # Add widgets to the left frame
+        self.hostname_label = tk.Label(self.left_frame, text="Nom de la machine : " + socket.gethostname())
+        self.hostname_label.pack()
+        self.ip_label = tk.Label(self.left_frame, text="Adresse IP : " + get_local_ip())
+        self.ip_label.pack()
+        self.public_ip_label = tk.Label(self.left_frame, text="Adresse IP publique : ")
+        self.public_ip_label.pack()
+        self.dns_hostname_label = tk.Label(self.left_frame, text="Nom de domaine : " + dns_hostname)
+        self.dns_hostname_label.pack()
+        self.after(5000, self.update_ip)
+        
+
+
+         # Add widgets to the middle frame
+        self.scan_button = tk.Button(self.middle_frame, text="Scan réseau", command=self.go_to_second_page)
+        self.scan_button.pack()
+        self.test_button = tk.Button(self.middle_frame, text="Lancer le test débit", command=self.go_to_third_page)
+        self.test_button.pack()
+
+
+        #Add Widgets to the right frame
+        self.ping_label = tk.Label(self.right_frame)
+        self.ping()
+        self.ping_label.pack()
+        self.resultat_label = tk.Label(self.right_frame, text="Résultat du dernier test de débit:")                 
+        self.resultat_label.pack()
+        self.download_label = tk.Label(self.right_frame, text=self.resultat_speedtest())
+        self.download_label.pack()
+        self.scan_result_button = tk.Button(self.middle_frame, text="Résultat scan réseau", command=self.go_to_four_page)
+        self.scan_result_button.pack()
+
+
+    def resultat_speedtest(self):
+        global result_download
+        global download
+        global upload
+        global result_upload
         try:
-            response = requests.get(agent_url, timeout=2)
-            variables = response.json()['variables']
+            with open(f"/home/client/semaos/speedtestresult.txt") as f:
+                download = f.readline().strip()
+                upload = f.readline().strip()
+                speedtest_result = " " + download + "\n" + " " + upload
+                result_download = download.split()[1]
+                result_upload = upload.split()[1]
+        except FileNotFoundError:
+        # Créer un nouveau fichier speedtestresult.txt
+            with open(f"/home/client/semaos/speedtestresult.txt", "w") as f:
+                f.write("Download speed: 0 Mbps\nUpload speed: 0 Mbps")
+            speedtest_result = "Download speed: 0 Mbps\nUpload speed: 0 Mbps"
+            result_download = 0
+            result_upload = 0
+        return speedtest_result
+
+
+   
+    def get_public_ip(self):
+        global ip
+        try:
+            urllib.request.urlopen('https://8.8.8.8', timeout=1) # Vérification de la connexion internet
+            response = requests.get("https://api.ipify.org")
+            response.raise_for_status()
+            ip = response.text
+            return response.text
+        except requests.exceptions.RequestException as e:
+            
+            return "Pas accès Internet"
+        except OSError:
+            
+            return "Pas accès Internet"
+        
+    def update_ip(self):
+        self.public_ip_label["text"] = "Adresse IP publique : " + self.get_public_ip()
+        self.after(5000, self.update_ip)
+    
+
+
+
+    
+
+    def ping(self):
+        global time_ping
+        output = os.popen("ping -c 1 8.8.8.8").read()
+        result = re.search("(time|temps)=(\d+)", output)
+        if result:
+            time_ping = float(result.groups()[1])
+            self.ping_label["text"] = "Ping: {} ms".format(time_ping)
+        else:        
+            time_ping = 'Failed'
+            self.ping_label["text"] = "Ping: Failed"
+        self.after(5000, self.ping)
+
+    def go_to_second_page(self):
+        self.destroy()
+        SecondPage(self.master)
+
+    def go_to_third_page(self):
+        self.destroy()
+        ThirdPage(self.master)
+
+    def go_to_four_page(self):
+        self.destroy()
+        FourPage(self.master)
+
+        
+class SecondPage(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        self.master.geometry("700x550")
+
+        self.scan()
+    def go_to_main_page(self):
+        self.destroy()
+        MainPage(self.master)
+
+    def create_widgets(self):
+
+        self.result_text = tk.Text(self)
+        self.result_text.pack()
+        self.result_text.tag_configure("right", justify='right')
+
+        self.back_button = tk.Button(self)
+        self.back_button["text"] = "Retour"
+        self.back_button["command"] = self.go_to_main_page
+        self.back_button.pack()
+
+        # Start the scan automatically when the page is displayed
+
+
+    def scan(self):
+        global local_ip
+        print(local_ip)
+        try:
+            urllib.request.urlopen('https://www.google.com')
         except:
-            variables = None
-        agent_vars.append((agent_ip, variables))
-    return render_template('home.html', agent_vars=agent_vars)
+            self.result_text.insert(tk.END, "Pas de connexion Internet\n")
+            return
 
-# Définition de la page d'affichage des variables d'un agent
-@app.route('/agent/<agent_ip>')
-def agent(agent_ip):
-    agent_url = f'http://{agent_ip}:5000/variables'
-    scan_results_url = f'http://{agent_ip}:5000/scan_results'
-    try:
-        response_variables = requests.get(agent_url, timeout=2)
-        variables = response_variables.json()
-        local_ip = variables.get('local_ip', None)
-        time_ping = variables.get('time_ping', None)
-        ip = variables.get('ip', None)
-        result_download = variables.get('download', None)
-        result_upload = variables.get('upload', None)
-        Hostname = variables.get('Hostname', None)
-        url_content = variables.get('url_content',None)
-        dns_hostname = variables.get('dns_hostname',None)
-    except:
-        variables = None
-        local_ip = None
-        time_ping = None
-        result_upload = None
-        result_download = None
-        ip = None
-        Hostname = None
-        url_content = None
-        dns_hostname = None
-    try:
-        response_scan_results = requests.get(scan_results_url, timeout=2)
-        scan_results = response_scan_results.json()
-        ips = scan_results.get('ips', None)
-        hostnames = scan_results.get('hostnames', None)
-        ports = scan_results.get('ports', None)
-    except:
-        scan_results = None
-        ips = None
-        hostnames = None
-        ports = None
+    # Détecter l'adresse IP locale et le sous-réseau
+        local_ip = get_local_ip()
+        subnet = ipaddress.ip_network(local_ip + "/24", strict=False)
+        print(subnet)
+        if not subnet:
+            self.result_text.insert(tk.END, "Impossible de détecter le sous-réseau\n")
+            return
 
-    return render_template('agent.html', agent_ip=agent_ip, variables=variables, local_ip=local_ip, time_ping=time_ping, result_upload=result_upload, result_download=result_download, ip=ip, Hostname=Hostname, ips=ips, hostnames=hostnames, ports=ports, scan_results=scan_results,url_content=url_content, dns_hostname=dns_hostname)
+        nm = nmap.PortScanner()
+        self.result_text.insert(tk.END, "Lancement de l'analyse\n")
+        self.master.update()
 
-# Route pour redémarrer un agent
-@app.route('/restart_agent', methods=['POST'])
-def restart_agent():
-    agent_ip = request.form['agent_ip']
-    agent_url = f'http://{agent_ip}:5000/restart'
-    try:
-        response = requests.post(agent_url, timeout=2)
-        result = 'Agent redémarré avec succès'
-    except:
-        result = 'Impossible de redémarrer l\'agent'
-    return redirect(url_for('home'))
+        time.sleep(2)
+        try:
+            nm.scan(str(subnet.network_address) + "/24", arguments='-sT -p1-1024')
 
-@app.route('/get_ping_value/<agent_ip>')
-def get_ping_value(agent_ip):
-    agent_url = f'http://{agent_ip}:5000/variables'
-    try:
-        response_variables = requests.get(agent_url, timeout=2)
-        variables = response_variables.json()
-        time_ping = variables.get('time_ping', None)
-    except:
-        time_ping = None
-    return {'time_ping': time_ping}
+            self.result_text.insert(tk.END, "Analyse terminée\n")
+            self.result_text.insert(tk.END, "\n")
+
+            self.result_text.insert(tk.END, "Adresse IP                      Hostname                         Ports\n")
+            self.result_text.insert(tk.END, "\n")
+
+            for host in nm.all_hosts():
+                try:
+                    hostnames = socket.gethostbyaddr(host)[0]
+                except socket.herror:
+                    hostnames = "unknown"
+                    ports = "Pas de port"
+                if "tcp" in nm[host] and nm[host]['tcp']:
+                    ports = ",".join([str(port) for port in nm[host]['tcp'].keys()])
+                    if ports == "":
+                        ports = "Pas de port"
+                self.result_text.insert(tk.END, "{:<30}{:<30}{}\n".format(host, hostnames, ports))
+                self.result_text.insert(tk.END, "\n")
+
+            with open(f'/home/client/semaos/scan_result.txt', 'w') as f:
+                f.write("Adresse IP\tHostname\tPorts\n")
+                for host in nm.all_hosts():
+                    try:
+                        hostnames = socket.gethostbyaddr(host)[0]
+                    except socket.herror:
+                        hostnames = "unknown"
+                    if "tcp" in nm[host] and nm[host]['tcp']:
+                        ports = ",".join([str(port) for port in nm[host]['tcp'].keys()])
+                        if ports == "":
+                            ports = "Pas de port"
+                    else:
+                        ports = "Pas de port"
+                    f.write("{}\t{}\t{}\n".format(host, hostnames, ports))
+        except Exception as e:
+            pass
 
 
-if __name__ == '__main__':
+
+
+
+
+
+
+
+class ThirdPage(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        self.master.geometry("700x450")
+        self.test_debit()
+    def create_widgets(self):
+        self.result_text = tk.Text(self)
+        self.result_text.pack()
+        self.back_button = tk.Button(self)
+        self.back_button["text"] = "Retour"
+        self.back_button["command"] = self.go_to_main_page
+        self.back_button.pack()
+
+    def go_to_main_page(self):
+        self.destroy()
+        MainPage(self.master)
+
+    def test_debit(self):
+
+
+        self.result_text.insert(tk.END, "Vérification de la connexion internet...\n")
+        
+        try:
+            urllib.request.urlopen("http://www.google.com", timeout=1)
+        except urllib.request.URLError:
+            self.result_text.delete(1.0, tk.END)
+            self.result_text.insert(tk.END, "Pas de connexion Internet\n")
+            return    
+        
+        self.result_text.delete(1.0, tk.END)
+        self.result_text.insert(tk.END, "Lancement du test de débit\n")
+        
+        self.master.update()
+        time.sleep(2)
+        # Lancer l'exécutable de Speedtest
+        process = subprocess.Popen(['speedtest', '--accept-license'], stdin=subprocess.PIPE)
+        process.communicate(input=b'yes\n')
+
+        result = subprocess.run(['speedtest'], stdout=subprocess.PIPE)
+
+        # Récupérer les résultats de la sortie
+        output = result.stdout.decode()
+
+        # Rechercher les valeurs de téléchargement et d'upload dans la sortie
+        download_match = re.search(r'(Download:\s*)(\d+\.?\d*) (Mbps)', output)
+        download = download_match.group(2)
+
+        upload_match = re.search(r'(Upload:\s*)(\d+\.?\d*) (Mbps)', output)
+        upload = upload_match.group(2)
+        # Afficher les résultats
+        self.result_text.insert(tk.END, "Test fini\n")
+        self.result_text.insert(tk.END, "Download: " + download + " Mbps\n") 
+        self.result_text.insert(tk.END, "Upload: " + upload + " Mbps")
+
+
+        #Ecrire les résultats dans un fichier txt
+        with open(f"/home/client/semaos/speedtestresult.txt", "w") as f:
+            f.write("Download: " + download + " Mbps\n") 
+            f.write("Upload: " + upload + " Mbps")
+        f.close()
+
+class FourPage(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+        self.master.geometry("800x600")
+
+    def go_to_main_page(self):
+        self.destroy()
+        MainPage(self.master)
+    
+
+    def create_widgets(self):
+        self.result_text = tk.Text(self, height=30, width=100)
+        self.result_text.tag_configure("center", justify='center')
+        self.result_text.tag_configure("right", justify='right')
+        self.result_text.pack()
+
+        self.back_button = tk.Button(self)
+        self.back_button["text"] = "Retour"
+        self.back_button["command"] = self.go_to_main_page
+        self.back_button.pack()
+        self.display_results()
+
+    def display_results(self):
+        try:
+            with open(f'/home/client/semaos/scan_result.txt', 'r') as f:
+                result = f.readlines()
+        except FileNotFoundError:
+            self.result_text.insert(tk.END, "Aucun résultat de scan réseau n'a été trouvé.\n")
+            return
+
+        for line in result:
+            ip, hostname, ports = line.strip().split('\t')
+            self.result_text.insert(tk.END, "\n")
+            self.result_text.insert(tk.END, "{:<20}{:<30}{}\n".format(ip, hostname, ports))
+
+
+
+        
+
+
+
+root = tk.Tk()
+root.attributes("-type", "tool")
+root.title("SemaOS "+ url_content)
+app = MainPage(master=root)
+
+
+
+def run_flask_app():
+    app = Flask(__name__)
+    @app.route('/')
+    def index():
+        return ''
+
+    @app.route('/variables')
+    def variables():
+        variables = {
+            'local_ip': local_ip,
+            'time_ping': time_ping,
+            'result_download': result_download,
+            'result_upload': result_upload,
+            'ip': ip,
+            'Hostname': Hostname,
+            'dns_hostname': dns_hostname,
+            'url_content': url_content           
+        }
+      
+        return jsonify(variables)
+
+
+    @app.route('/restart', methods=['POST'])
+    def restart():
+        try:
+            subprocess.run(['reboot'])
+            return jsonify({'status': 'success'})
+        except Exception as e:
+            return jsonify({'status': 'error', 'message': str(e)})
+        
+    # Send the results to the client using the Flask web server
+    
+    @app.route('/scan_results')
+    def scan_results():
+        with open('/home/client/semaos/scan_result.txt', 'r') as f:
+            result = f.readlines()
+
+    # Create empty lists to store the IP, hostname, and port results
+        ips = []
+        hostnames = []
+        ports = []
+
+        for line in result:
+            ip_scan, hostname_scan, ports_scan = line.strip().split('\t')
+            ips.append(ip_scan)
+            hostnames.append(hostname_scan)
+            ports.append(ports_scan)
+
+        scan_results = {
+            'ips': ips,
+            'hostnames': hostnames,
+            'ports': ports
+        }
+
+        return jsonify(scan_results)
+
+
+
+
     app.run(host='0.0.0.0', port=5000)
-```
-message.txt
-4 Ko
+
+# Lancer le thread pour l'application Flask
+flask_thread = threading.Thread(target=run_flask_app)
+flask_thread.start()
+app.mainloop()
